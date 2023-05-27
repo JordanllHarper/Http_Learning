b37acf2e9ec3c9eacbbdebab7599e4056b2e3c46
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2023-05-27 18:37:33 +0100

    Basic information in request

--- http/src/HttpRequest.rs
+++ http/src/HttpRequest.rs
@@ -11,9 +11,17 @@ pub struct BasicInfo {
     version: String,
 }
 impl BasicInfo {
-    pub fn new(verb: Verb, uri: String, version: String) {
+    pub fn new(verb: Verb, uri: String, version: String) -> BasicInfo {
         BasicInfo { verb, uri, version }
     }
 }
 
-pub struct HttpRequest {}
+//TODO: Add more fields
+pub struct HttpRequest {
+    basic_info: BasicInfo,
+}
+impl HttpRequest {
+    pub fn new(basic_info: BasicInfo) -> HttpRequest {
+        HttpRequest { basic_info }
+    }
+}
